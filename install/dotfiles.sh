5b40aad0a5893797aad9f56ee29d62dfe41d7f92
Buzzycaf <aferrer@gmail.com>
2026-01-09 21:01:42 -0800

    removed legacy files not needed

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -124,11 +124,10 @@ dotfiles_link_all() {
     fi
   fi
 
-  # Fuzzel config
-  log "Ensuring fuzzel launcher configuration is present"
-  run "mkdir -p '$target_home/.config/fuzzel'"
-  [[ ! -e "$target_home/.config/fuzzel/fuzzel.ini" ]] && \
-    run "ln -s '$REPO_DIR/fuzzel/fuzzel.ini' '$target_home/.config/fuzzel/fuzzel.ini'"
+  # fuzzel
+  if [[ -f "$REPO_DIR/fuzzel/fuzzel.ini" ]]; then
+    link_file "$REPO_DIR/fuzzel/fuzzel.ini" "$target_home/.config/fuzzel/fuzzel.ini"
+  fi
   
   # Save Backups
   [[ -d "$BACKUP_DIR" ]] && log "Backups saved in: $BACKUP_DIR"
